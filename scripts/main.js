ead53e8b90d34cc69a629506d8eb889e4fc30e9e
Kunal M.A. <kunalma23@gmail.com>
2025-03-11 02:30:24 +0530

    feat: add contact form popup

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,18 @@
 particlesJS.load("particles-js", "config/particles.json", function () {
     console.log("[Loaded] Config for Particles.js");
 });
+
+function connectForm() {
+    document.getElementById("blurBox").style.visibility = "visible";
+    document.getElementById("contactForm").style.visibility = "visible";
+}
+
+function closePopup() {
+    document.getElementById("blurBox").style.visibility = "hidden";
+    if (document.getElementById("contactForm").style.visibility == "visible") {
+        document.getElementById("contactForm").style.visibility = "hidden";
+    }
+    else {
+        console.log("No popup to close");
+    }
+}
